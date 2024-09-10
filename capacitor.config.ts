65866b6125b847e5b58f08c203a15d1205dd3919
Stellce <andrey.is323@gmail.com>
2024-09-10 23:53:21 +0200

    Update to Angular 18

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ionic.traveller',
   appName: 'Traveller',
-  webDir: 'www',
+  webDir: 'www/browser',
   server: {
     androidScheme: 'https'
   }
